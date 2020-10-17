40dec1c985b28c2abb329ecde543fcf6b88bb630
Andrew Mason <andrewmcodes@protonmail.com>
2020-10-17 03:15:44 -0400

    chore: initial commit

---
+++ lib/forem/client.rb
@@ -0,0 +1,8 @@
+require "forem/client/version"
+
+module Forem
+  module Client
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/forem/client_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Forem::Client do
+  it "has a version number" do
+    expect(Forem::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
